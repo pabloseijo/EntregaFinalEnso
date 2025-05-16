895fd94abd9618efd578a1b238b562a5d02e2b72
Sócrates <142091962+soocratess@users.noreply.github.com>
2025-05-16 11:27:13 +0200

    Sprint2 (#3)

--- src/ListaReproduccion/IListaReproduccion.java
+++ src/ListaReproduccion/IListaReproduccion.java
@@ -9,8 +9,19 @@ import usuario.IUsuario;
 
 public interface IListaReproduccion {
 	public int stringASegundos(String duracion);
+
 	public Map<IUsuario, List<ICancion>> generarListasPorUsuario(String rutaCSV);
+
 	public int calcularDuracion(List<ICancion> canciones);
-	public List<ICancion> intercalarCanciones(Map<IUsuario, List<ICancion>> listasUsuario, Set<IUsuario> usuariosEnCoche);
-	public List<Integer> generarListaReproduccionIntercalada(String rutaCSV, Set<IUsuario> usuariosEnCoche, int maxTiempo);
+
+	public List<ICancion> intercalarCanciones(Map<IUsuario, List<ICancion>> listasUsuario,
+			Set<IUsuario> usuariosEnCoche);
+
+	public List<Integer> generarListaReproduccionIntercalada(String rutaCSV, Set<IUsuario> usuariosEnCoche,
+			int maxTiempo);
+
+	// ***** Nuevas operaciones Sprint 2 *****
+	List<ICancion> filtrarPorDuracion(List<ICancion> canciones, int minSeg, int maxSeg);
+
+	List<ICancion> ordenarPorDuracion(List<ICancion> canciones, String orden);
 }
